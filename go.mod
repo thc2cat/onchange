b245e218cc5c19fcfb363c3ac288dc39886afb87
thc2cat <thc2cat+github@gmail.com>
2026-06-08 15:02:15 +0200

    race condition

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
 module projects/onchange
 
-go 1.15
+go 1.24.5
 
-require (
-	github.com/fsnotify/fsnotify v1.4.7
-	golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
-)
+require github.com/fsnotify/fsnotify v1.4.7
+
+require golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
--- main.go
+++ main.go
@@ -7,114 +7,98 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-var (
-	watcher  *fsnotify.Watcher
-	command  string
-	interval = (int64)(2)
-)
+var interval = int64(2)
 
 func main() {
-
-	var err error
-
-	for i := 1; i < len(os.Args); i++ {
-		command += os.Args[i]
-		command += ""
+	// Reconstruction propre de la commande
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go-onchange <command>")
 	}
+	command := strings.Join(os.Args[1:], " ")
 
-	// creates a new file watcher
-	watcher, err = fsnotify.NewWatcher()
+	// Création du watcher
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer watcher.Close()
 
-	// starting at the root of the project,
-	// walk each file/directory searching for
-	// directories
 	pwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	go watchPeriodically(pwd, 10)
+
+	// Lancement de la surveillance périodique
+	go watchPeriodically(watcher, pwd, 10)
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
 
+	// Boucle principale d'événements
 	go func() {
 		timestamp := time.Now().Unix()
 		for {
 			select {
-			// watch for events
-			// case event := <-watcher.Events:
-			// fmt.Printf("EVENT! %#v\n", event)
 			case <-watcher.Events:
 				if time.Now().Unix()-timestamp > interval {
 					execcmd(command)
 					timestamp = time.Now().Unix()
 				}
-
-			// watch for errors
 			case err := <-watcher.Errors:
 				fmt.Println("ERROR", err)
 			}
 		}
 	}()
 
+	// fmt.Printf("Surveillance démarrée pour : %s\n", command)
 	<-done
 }
 
-// watchDir gets run as a walk func, searching for directories to add watchers to
-func watchDir(path string, fi os.FileInfo, err error) error {
-	// since fsnotify can watch all the files in a directory, watchers only need
-	// to be added to each nested directory
-	if fi.Mode().IsDir() {
-		return watcher.Add(path)
+// watchDir utilise le watcher passé en paramètre
+func watchDir(watcher *fsnotify.Watcher) filepath.WalkFunc {
+	return func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			// Ignore le dossier .git pour éviter trop d'événements
+			if strings.Contains(path, ".git") {
+				return nil
+			}
+			return watcher.Add(path)
+		}
+		return nil
 	}
-	return nil
 }
 
-// watchPeriodically adds sub directories periodically to watch, with the help
-// of fsnotify which maintains a directory map rather than slice.
-func watchPeriodically(directory string, interval int) {
-	printedOnce := false
-	done := make(chan struct{})
-	go func() {
-		done <- struct{}{}
-	}()
+func watchPeriodically(watcher *fsnotify.Watcher, directory string, interval int) {
 	ticker := time.NewTicker(time.Duration(interval) * time.Second)
 	defer ticker.Stop()
-	for ; ; <-ticker.C {
-		<-done
-		if err := filepath.Walk(directory, watchDir); err != nil {
-			if !printedOnce {
-				fmt.Fprintln(os.Stderr, "Error with filepath.Walk on", directory, err)
-				printedOnce = true
-			}
-		}
-		go func() {
-			done <- struct{}{}
-		}()
+
+	// Premier passage immédiat
+	_ = filepath.Walk(directory, watchDir(watcher))
+
+	for range ticker.C {
+		_ = filepath.Walk(directory, watchDir(watcher))
 	}
 }
 
 func execcmd(c string) {
-
 	time.Sleep(time.Duration(interval) * time.Second / 2)
 	hr, min, sec := time.Now().Clock()
 
-	fmt.Printf("%0d:%02d:%02d - %s\n", hr, min, sec, c)
-	r, err := exec.Command("bash", "-c", c).CombinedOutput() // Why worry ?
+	fmt.Printf("%0d:%02d:%02d - Exécution : %s\n", hr, min, sec, c)
+	r, err := exec.Command("bash", "-c", c).CombinedOutput()
 	if err != nil {
-		log.Print(err)
+		log.Printf("Erreur d'exécution : %v", err)
 	}
-	if err == nil && len(r) > 0 {
+	if len(r) > 0 {
 		fmt.Fprintf(os.Stderr, "%s\n", r)
 	}
 }
